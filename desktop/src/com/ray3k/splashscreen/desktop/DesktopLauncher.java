71aa226f38d526980d0f700861812a368dda30fc
Raymond Buckley <raymond.ray3k@gmail.com>
2017-09-25 09:36:01 -0700

    Initial project files.

---
+++ desktop/src/com/ray3k/splashscreen/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ray3k.splashscreen.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ray3k.splashscreen.Core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Core(), config);
+	}
+}
